6e1393d21b2a308ba04ffc1d02ed8a4b7e1db71f
KovalyovaMaria <idKovalyova@gmail.com>
2025-10-17 19:09:12 +0300

    Initial commit

--- data/photoSocialMedia.js
+++ data/photoSocialMedia.js
@@ -1,8 +1,8 @@
 const photoSocialMedia = [
-    {id: 1, imgUrl: "assets/img/img_photo_one.jpg"},
-    {id: 2, imgUrl: "assets/img/img_photo_two.png"},
-    {id: 3, imgUrl: "assets/img/img_photo_three.png"},
-    {id: 4, imgUrl: "assets/img/img_photo_four.png"},
-    {id: 5, imgUrl: "assets/img/img_photo_five.jpg"},
+    {id: 1, imgUrl: "/Moon-River-/assets/img/img_photo_one.jpg"},
+    {id: 2, imgUrl: "/Moon-River-/assets/img/img_photo_two.png"},
+    {id: 3, imgUrl: "/Moon-River-/assets/img/img_photo_three.png"},
+    {id: 4, imgUrl: "/Moon-River-/assets/img/img_photo_four.png"},
+    {id: 5, imgUrl: "/Moon-River-/assets/img/img_photo_five.jpg"},
 ];
 export default photoSocialMedia;
